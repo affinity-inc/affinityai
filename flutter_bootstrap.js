1a3cb12fba03ad6508fd24c39191f7fc447a4306
Affinity Inc <pandeyanita4472@gmail.com>
2024-12-03 15:54:45 +0530

    release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2554300842"
+    serviceWorkerVersion: "2181683089"
   }
 });
